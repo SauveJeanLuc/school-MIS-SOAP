b323a9e74e7e7f2ed4a827e60fbffc1d7b332ccb
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-06-22 09:18:06 +0200

    project structure

---
+++ src/main/java/com/edu/schoolsoapapi/SchoolSoapApiApplication.java
@@ -0,0 +1,13 @@
+package com.edu.schoolsoapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSoapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolSoapApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/schoolsoapapi/SchoolSoapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.schoolsoapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSoapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
